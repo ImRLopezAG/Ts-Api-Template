5306b0d0c0b56d3d9809abeff21206da1c66e46d
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2023-04-09 03:33:10 -0400

    Upgrade validation token

--- src/libs/jwtvalidation.ts
+++ src/libs/jwtvalidation.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response } from 'express'
-import jwt from 'jsonwebtoken'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 import { SECRET } from '../types/constants'
 
 export const jwtValidation = (req: Request, res: Response, next: NextFunction): Response | any => {
   const token = req.header('auth-token')
-  if (token === undefined) return res.status(401).json({ error: 'Access denied' })
+  if (token === undefined) return res.status(401).json({ error: 'Access denied, you need to login' })
   try {
-    jwt.verify(token, SECRET)
+    const payload = jwt.verify(token, SECRET) as JwtPayload
+    req.uui = payload.id
     next()
   } catch (error) {
-    res.status(400).json('Invalid token')
+    if (error instanceof Error) {
+      return res.status(500).json({ error: 'Error while validating token' })
+    }
+    return res.status(400).json({ error: 'Invalid token' })
   }
 }
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,5 +1,5 @@
 declare namespace Express {
   export interface Request {
-    user?: any
+    uui: string
   }
 }
